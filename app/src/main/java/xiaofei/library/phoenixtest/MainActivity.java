b806dd0b18ed8ee5a81506e7850e78838e8be932
Xiaofei <xiaofei.it@gmail.com>
2016-11-28 15:50:17 +0800

    Add more tests

--- app/src/main/java/xiaofei/library/phoenixtest/MainActivity.java
+++ app/src/main/java/xiaofei/library/phoenixtest/MainActivity.java
@@ -3,7 +3,12 @@ package xiaofei.library.phoenixtest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.gson.Gson;
+
+import java.lang.reflect.GenericSignatureFormatError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +24,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        BlankFragment fragment = BlankFragment.newInstance("s1", "s2");
+        Gson gson = new Gson();
+        //Log.v("Eric", "Fragment = " + gson.toJson(fragment));
+        //Log.v("Eric", "Activity = " + gson.toJson(this)); // block
+        //Log.v("Eric", "Class = " + gson.toJson(MainActivity.class)); // throw an exception:  Forgot to register a type adapter?
+
     }
 }
--- app/src/main/java/xiaofei/library/phoenixtest/SecondActivity.java
+++ app/src/main/java/xiaofei/library/phoenixtest/SecondActivity.java
@@ -3,20 +3,31 @@ package xiaofei.library.phoenixtest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v("Eric", "si = " + savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Intent intent = getIntent();
+        setIntent(null);
+        Intent intent1 = getIntent();
         StringBuilder sb = new StringBuilder();
-        sb.append("intent = " + intent);
+        sb.append("intent = " + intent + " intent1 = " + intent1);
         if (intent != null) {
             sb.append(" Int = ").append(intent.getIntExtra("Int", -1));
         }
         Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show();
+        findViewById(R.id.b).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
