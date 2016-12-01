25df2069adbcdacd63cd73b7eb8eea78f9a16199
Xiaofei <xiaofei.it@gmail.com>
2016-12-01 22:11:40 +0800

    temp

---
+++ phoenix/src/main/java/xiaofei/library/phoenix/visitor/SetBundleVisitor.java
@@ -0,0 +1,61 @@
+/**
+ *
+ * Copyright 2016 Xiaofei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package xiaofei.library.phoenix.visitor;
+
+import android.os.Bundle;
+
+import java.util.Collection;
+import java.util.Set;
+
+import xiaofei.library.phoenix.Phoenix;
+
+/**
+ * Created by Xiaofei on 16/12/1.
+ */
+
+abstract class SetBundleVisitor implements BundleVisitor {
+
+    private Set mInstance;
+
+    SetBundleVisitor(Set instance) {
+        mInstance = instance;
+    }
+
+    @Override
+    public void put(Bundle bundle, String key, Object value) {
+        if (value != null) {
+            Bundle tmp1 = new Bundle();
+            int i = 0;
+            for (Object o : mInstance) {
+                Bundle tmp2 = new Bundle();
+                Phoenix.saveInstance(o, tmp2);
+                tmp1.putBundle(Integer.toString(i++), tmp2);
+            }
+            tmp1.putInt("Size", ++i);
+            bundle.putBundle(key, tmp1);
+        }
+    }
+
+    @Override
+    public Object get(Bundle bundle, String key) {
+        Bundle tmp1 = bundle.getBundle(key);
+        //int size = bundle.
+        return bundle.getBoolean(key);
+    }
+}
